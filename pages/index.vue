6831b0d74d6225f6fc6019f4a3401955af19ca09
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2022-12-18 02:14:37 +0200

    add index.vue

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+
+<template>
+  <div>
+    <div class="p-70"></div>
+    <About />
+  </div>
+</template>
+
+
+
+<style lang="sass" scoped>
+.p-70
+    padding: 70px
+</style>
