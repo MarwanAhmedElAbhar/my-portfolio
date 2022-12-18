e5b1ce61909b82e5eb0d4a28ee2fac24f23418f5
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2022-12-18 02:16:57 +0200

    initialize about.vue

---
+++ components/about.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <div class="d-flex">
+    <div class="col-7">
+      <div class="d-flex align-items-baseline">
+        <h1 class="mr-2">Hello World, I'm</h1>
+        <span class="text-secondary fs-28">Marwan Ahmed</span>
+      </div>
+    </div>
+    <div class="col-4">
+        <img class="img-size" src="@/assets/images/man-working.jpeg" alt="man-working">
+    </div>
+  </div>
+</template>
+
+
+<style lang="sass" scoped>
+.fs-28
+    font-size: 28px
+.img-size
+    max-width: 100% 
+    height: auto   
+</style>
