656c44c0fb966a4a19341e1ef13ee56c27bc2f58
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-01 00:57:27 +0200

    add tag component

---
+++ components/tag.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { tag } = defineProps({
+  tag: Object,
+});
+</script>
+
+<template>
+  <p class="tag-style mr-2" :style="{ backgroundColor: tag.backgroundColor }">
+    {{ tag.name }}
+  </p>
+</template>
+
+<style lang="sass" scoped>
+.tag-style
+    color: white
+    font-weight: 600
+    padding: 3px 6px
+    border-radius: 12px
+</style>
