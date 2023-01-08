7ed9cd01e4e51df43ba0710179144db435f04852
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-08 23:48:02 +0200

    add main contact component

---
+++ components/contact/main.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container py-5">
+    <div class="row">
+    <ContactInformation  class="col-lg-5 col-12"/>
+    <ContactForm class="col-lg-7 col-12"/>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+</style>
