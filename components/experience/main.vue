478ba7dc350a190d56fff1d4cafdb3bd49b7d399
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-08 23:52:10 +0200

    add main experiences component

---
+++ components/experience/main.vue
@@ -0,0 +1,24 @@
+<script setup>
+import data from "./data";
+
+const experiences = computed(() => data);
+</script>
+
+<template>
+  <div class="py-5 container">
+    <h1 class="mb-5">Experiences</h1>
+    <div
+      class="timeline"
+      v-for="experience in experiences"
+      :key="experience.id"
+    >
+      <ExperienceContent :experience="experience" />
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.timeline
+  position: relative
+  margin-left: 20px
+</style>
