02fa38383bda3cac1ed2a8574243daa781053734
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-08 23:52:32 +0200

    add experiences data

---
+++ components/experience/data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        name: "Arabyads",
+        logo: "",
+        period: "(12/2021 - current)",
+        description: "Developed Boostiny web application"
+    },
+    {
+        id: 2,
+        name: "IPMagix",
+        logo: "",
+        period: "(02/2020 - 12/2021)",
+        description: "Worked on multiple web applications"
+    },
+  
+]
