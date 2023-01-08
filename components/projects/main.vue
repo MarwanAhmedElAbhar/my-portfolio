ccd45f38ac71cad52e4d2279355120d349235a59
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-08 23:46:48 +0200

    add main projects component

---
+++ components/projects/main.vue
@@ -0,0 +1,24 @@
+<script setup>
+import data from "./data"
+
+const projects = computed(() => data)
+</script> 
+ 
+<template>
+  <div class="py-5 container">
+    <h1 class="mb-4">Projects</h1>
+    <div class="row align-items-center justify-content-center no-gutters">
+      <ProjectsCard
+        v-for="project in projects"
+        :key="project.id"
+        :project="project"
+        class="col-md-3 col-sm-6 col-12 my-2"
+      />
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="sass" scoped>
+</style>
