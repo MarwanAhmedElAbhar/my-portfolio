aa64ebdd79eb218d43f315d2cb842a2e1fb4d56d
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-08 23:50:43 +0200

    add contact information component

---
+++ components/contact/information.vue
@@ -0,0 +1,48 @@
+<script setup>
+</script>
+
+<template>
+  <div class="contact-info">
+    <div class="mb-3">
+      <p class="info-header">Interested to work together?</p>
+      <p class="info-header">Get in touch.</p>
+    </div>
+    <p class="info-text">
+      Would be my pleasure to serve if the right project comes along, Feel free
+      to contact me.
+    </p>
+    <div class="details-container">
+    <div class="d-flex align-items-center">
+      <img src="~/assets/img/map.svg" alt="" />
+      <p class="ml-2 info-details">Mohandeseen, Giza, Egypt</p>
+    </div>
+    <div class="d-flex align-items-center mt-4">
+      <img src="~/assets/img/mail.svg" alt="" />
+      <p class="ml-2 info-details">marwan.a.elabhar@gmail.com</p>
+    </div>
+      <div class="d-flex align-items-center mt-4">
+      <img src="~/assets/img/phone.svg" alt="" />
+      <p class="ml-2 info-details">+201000632144</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.contact-info
+    background: #f7f7ff
+    padding: 30px
+.info-header
+    font-size: 28px
+    font-weight: 600
+.info-text
+    color: #555
+    font-weight: 400
+    margin-top: 25px
+.info-details
+    font-size: 16px
+    font-weight: 500
+    line-height: 1.3
+.details-container
+    margin-top: 50px        
+</style>
