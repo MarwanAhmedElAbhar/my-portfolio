aa7967c3bf40ae6af562eea56fed384edc6ce9a0
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-15 23:38:15 +0200

    add gap top projects cards

--- components/projects/main.vue
+++ components/projects/main.vue
@@ -7,7 +7,7 @@ const projects = computed(() => data)
 <template>
   <div class="py-5 container">
     <h1 class="mb-4">Projects</h1>
-    <div class="row align-items-center justify-content-center no-gutters">
+    <div class="row align-items-center justify-content-center no-gutters gap-30">
       <ProjectsCard
         v-for="project in projects"
         :key="project.id"
@@ -21,4 +21,6 @@ const projects = computed(() => data)
 
 
 <style lang="sass" scoped>
+.gap-30
+  gap: 30px
 </style>
