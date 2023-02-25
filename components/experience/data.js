f521f8a787c42fd37762dffd87f3149aef2c4177
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-02-25 14:50:25 +0200

    update the experience description

--- components/experience/data.js
+++ components/experience/data.js
@@ -2,16 +2,16 @@ export default [
     {
         id: 1,
         name: "Arabyads",
-        logo: "",
+        logo: "~/assets/img/ArabyAds-Logo.jpeg",
         period: "(12/2021 - current)",
-        description: "Developed Boostiny web application"
+        description: "Worked on the internal team's dashboard. Contributed to sprints planning and grooming. Reviewed my teammates PRs. Worked on maintaining and improving the team's codebase, along with my teammates."
     },
     {
         id: 2,
         name: "IPMagix",
-        logo: "",
+        logo: "~/assets/ipmagix.jpeg",
         period: "(02/2020 - 12/2021)",
-        description: "Worked on multiple web applications"
+        description: "Worked on external projects (dashboards) including the dashboard for Cairo ICT event. Worked on web portals for wi-fi connections. Created a demo for G20 event for video meetings, along with my teammates."
     },
-  
+
 ]
