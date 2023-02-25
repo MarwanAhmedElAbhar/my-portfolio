0b7bde2fe4781a39851a5dafeb5d5410c27af322
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-02-25 14:31:37 +0200

    update linkedin profile link

--- components/contact/information.vue
+++ components/contact/information.vue
@@ -12,43 +12,48 @@
       to contact me.
     </p>
     <div class="details-container">
-    <div class="d-flex align-items-center px-2 word-break">
-      <img src="~/assets/img/map.svg" alt="" />
-      <p class="ml-2 info-details">Mohandeseen, Giza, Egypt</p>
-    </div>
-    <div class="d-flex align-items-center mt-4 px-2 word-break">
-      <img src="~/assets/img/mail.svg" alt="" />
-      <p class="ml-2 info-details">marwan.a.elabhar@gmail.com</p>
-    </div>
+      <div class="d-flex align-items-center px-2 word-break">
+        <img src="~/assets/img/map.svg" alt="map-image" />
+        <p class="ml-2 info-details">Mohandeseen, Giza, Egypt</p>
+      </div>
       <div class="d-flex align-items-center mt-4 px-2 word-break">
-      <img src="~/assets/img/phone.svg" alt="" />
-      <p class="ml-2 info-details">+201000632144</p>
-    </div>
-       <div class="d-flex align-items-center mt-4 px-2 word-break">
-      <img src="~/assets/img/linkedin.svg" alt="" />
-      <a class="ml-2 info-details linkedin-profile" href="https://www.linkedin.com/in/marwan-ahmed3 ">LinkedIn Profile</a>
-    </div>
+        <img src="~/assets/img/mail.svg" alt="mail-image" />
+        <p class="ml-2 info-details">marwan.a.elabhar@gmail.com</p>
+      </div>
+      <div class="d-flex align-items-center mt-4 px-2 word-break">
+        <img src="~/assets/img/phone.svg" alt="phone-image" />
+        <p class="ml-2 info-details">+201000632144</p>
+      </div>
+      <div class="d-flex align-items-center mt-4 px-2 word-break">
+        <img src="~/assets/img/linkedin.svg" alt="linkedin-image" />
+        <a
+          class="ml-2 info-details linkedin-profile"
+          href="https://www.linkedin.com/in/marwanelabhar"
+          target="_blank"
+          >LinkedIn Profile</a
+        >
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="sass" scoped>
 .contact-info
-    background: #f7f7ff
-    padding: 30px
+  background: #f7f7ff
+  padding: 30px
 .info-header
-    font-size: 28px
-    font-weight: 600
+  font-size: 28px
+  font-weight: 600
 .info-text
-    color: #555
-    font-weight: 400
-    margin-top: 25px
+  color: #555
+  font-weight: 400
+  margin-top: 25px
 .info-details
-    font-size: 16px
-    font-weight: 500
-    line-height: 1.3
+  font-size: 16px
+  font-weight: 500
+  line-height: 1.3
 .details-container
-    margin-top: 50px   
+  margin-top: 50px
 .linkedin-profile
-  color: #000         
+  color: #000
 </style>
