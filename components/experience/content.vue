5d3966708fd7039e926a117ff382ef1e79aadf37
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-03-10 23:10:05 +0200

    modify description display

--- components/experience/content.vue
+++ components/experience/content.vue
@@ -9,40 +9,45 @@ const { experience } = defineProps({
 <template>
   <div class="content">
     <div class="d-flex align-items-center mb-2 flex-wrap">
-      <img :src="experience.logo" alt="company-logo" class="logo-size mr-2">
+      <img :src="experience.logo" alt="company-logo" class="logo-size mr-2" />
       <div class="d-flex align-items-baseline">
-      <p class="experience-name mr-2">{{ experience.name }}</p>
-      <p class="experience-period">{{ experience.period }}</p>
+        <p class="experience-name mr-2">{{ experience.name }}</p>
+        <p class="experience-period">{{ experience.period }}</p>
       </div>
     </div>
-    <p class="experience-description">{{ experience.description }}</p>
+    <ul
+      v-for="(experience, index) in experience.description"
+      :key="index"
+    >
+      <li class="experience-description">{{ experience }}</li>
+    </ul>
   </div>
 </template>
 
 <style lang="sass" scoped>
 .content
-    border-left: 3px solid #474559
-    padding-left: 30px
-    padding-bottom: 50px
-    &::before
-        content: ''
-        width: 25px
-        height: 25px
-        background-color: #474559
-        border-radius: 50%
-        position: absolute
-        left: -10px
+  border-left: 3px solid #474559
+  padding-left: 30px
+  padding-bottom: 50px
+  &::before
+    content: ''
+    width: 25px
+    height: 25px
+    background-color: #474559
+    border-radius: 50%
+    position: absolute
+    left: -10px
 .experience-name, .experience-period
-    color: #474559
-    font-weight: bold
+  color: #474559
+  font-weight: bold
 .logo-size
   width: 80px
 .experience-name
-    font-size: 26px
+  font-size: 26px
 .experience-period
-    font-size: 16px
+  font-size: 16px
 .experience-description
-    font-size: 18px
-    color: #5b5b5b
-    line-height: 1.5
+  font-size: 18px
+  color: #5b5b5b
+  line-height: 1.5
 </style>
--- components/experience/data.js
+++ components/experience/data.js
@@ -4,14 +4,15 @@ export default [
         name: "Arabyads",
         logo: "/img/ArabyAds-Logo.jpeg",
         period: "(12/2021 - current)",
-        description: "Worked on the internal team's dashboard. Contributed to sprints planning and grooming. Reviewed my teammates PRs. Worked on maintaining and improving the team's codebase, along with my teammates."
+        description: ["Worked on the internal team's dashboard.",
+        "Contributed to sprints planning and grooming.", "Reviewed my teammates PRs.", "Worked on maintaining and improving the team's codebase, along with my teammates."]
     },
     {
         id: 2,
         name: "IPMagix",
         logo: "/img/ipmagix.jpeg",
         period: "(02/2020 - 12/2021)",
-        description: "Worked on external projects (dashboards) including the dashboard for Cairo ICT event. Worked on web portals for wi-fi connections. Created a demo for G20 event for video meetings, along with my teammates."
+        description: ["Worked on external projects (dashboards) including the dashboard for Cairo ICT event.", "Worked on web portals for wi-fi connections.", "Created a demo for G20 event for video meetings, along with my teammates."]
     },
 
 ]
